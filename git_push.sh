10ab636dc4de316828794da00c9caa646546652b
lotatachukwu <lotaodi46@gmail.com>
2023-11-14 18:15:49 +0100

    push

--- git_push.sh
+++ git_push.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 # Navigate to the project directory
-cd my_directory
+cd my directory
 
-# Add all changes
-git add .
+# List untracked and modified files
+git status -s
 
-# Commit with a default message or take a message as an argument
-git commit -m "${1:-'Automated commit'}"
+# Ask the user for the file to add
+read -p "Enter the file you want to add (or press Enter to add all): " file_to_add
+
+# Add the chosen file or all files
+if [ -z "$file_to_add" ]; then
+    git add .
+else
+    git add "$file_to_add"
+fi
+
+# Ask the user for the commit message
+read -p "Enter the commit message: " commit_message
+
+# Commit with the entered message or a default message
+git commit -m "${commit_message:-'Automated commit'}"
 
 # Push changes to the remote repository
 git push
+
