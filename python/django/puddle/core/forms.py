6ff7a9fe52813bce2e361a58d56d6b6420897134
lotachukwu46 <lotaodi46@gmail.com>
2024-05-19 17:41:12 +0100

    'Updated'

--- python/django/puddle/core/forms.py
+++ python/django/puddle/core/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    class meta:
+    class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
